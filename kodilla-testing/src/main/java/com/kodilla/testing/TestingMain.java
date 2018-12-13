adb2f11a1f653afd15fb7a9788f8cea341a3d763
Luke1024 <chajdas.lukasz@gmail.com>
2018-12-13 10:57:27 +0100

    Third Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calc = new Calculator();
+        int result = calc.add(5,3);
+
+        if(result==8){
+            System.out.println("Adding ok.");
+        } else {
+            System.out.println("Adding error.");
+        }
+
+        result = calc.subtract(5,3);
+
+        if(result==2){
+            System.out.println("Subtracting ok.");
+        } else {
+            System.out.println("Subtracting error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int add(int a, int b){
+        int sum=a+b;
+        return sum;
+    }
+
+    public int subtract(int a, int b){
+        int difference=a-b;
+        return difference;
+    }
+}
