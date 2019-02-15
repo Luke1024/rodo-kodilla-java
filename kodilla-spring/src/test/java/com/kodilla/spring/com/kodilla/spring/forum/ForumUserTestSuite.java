52e314c62302c75498da5d7f83721c4699e66dd7
Luke1024 <chajdas.lukasz@gmail.com>
2019-02-15 13:56:52 +0100

    task 14.1

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.spring.com.kodilla.spring.forum;
+
+import com.kodilla.spring.forum.ForumUser;
+import com.kodilla.spring.shape.Shape;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class ForumUserTestSuite {
+
+    @Test
+    public void testGetUsername(){
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+        //When
+        String userName = forumUser.getUsername();
+        //Then
+        Assert.assertEquals("John Smith", userName);
+    }
+}
