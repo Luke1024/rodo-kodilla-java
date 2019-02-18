f817287a3657d5c983152c903600d5e5de0094e5
Luke1024 <chajdas.lukasz@gmail.com>
2019-02-18 15:52:58 +0100

    task 14.2

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -5,29 +5,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class Calculator {
     private final Display display;
-    private double result;
 
     public Calculator(final Display display){
         this.display = display;
     }
 
     public double add(double a, double b){
-        result = a + b;
+        double result = a + b;
         display.displayValue(result);
         return result;
     }
     public double sub(double a, double b){
-        result = a - b;
+        double result = a - b;
         display.displayValue(result);
         return result;
     }
     public double mul(double a, double b){
-        result = a * b;
+        double result = a * b;
         display.displayValue(result);
         return result;
     }
     public double div(double a, double b){
-        result = a / b;
+        double result = a / b;
         display.displayValue(result);
         return result;
     }
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,15 +1,11 @@
 package com.kodilla.spring.calculator;
 
-import com.kodilla.spring.SpringRunner;
+
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class CalculatorTestSuite {
 
     @Test
