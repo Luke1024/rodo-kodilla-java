852f131827de6ca09f2db22136df492e394e7958
Luke1024 <chajdas.lukasz@gmail.com>
2019-03-11 19:36:08 +0100

    task 14.3 fix

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,14 +1,28 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class BoardConfig {
+    @Autowired
+    @Qualifier("toDoList")
+    private TaskList toDolist;
+
+    @Autowired
+    @Qualifier("inProgressList")
+    private TaskList inProgressList;
+
+    @Autowired
+    @Qualifier("doneList")
+    private TaskList doneList;
+
     @Bean
     public Board getBoard() {
-        return new Board(getToDo(), getInProgress(), getDone());
+        return new Board(toDolist, inProgressList, doneList);
     }
 
     @Bean(name = "toDoList")
@@ -20,7 +34,6 @@ public class BoardConfig {
     public TaskList getInProgress() {
         return new TaskList();
     }
-
     @Bean(name = "doneList")
     public TaskList getDone() {
         return new TaskList();
--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -2,20 +2,23 @@ package com.kodilla.spring.portfolio;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class BoardTestSuite {
+    @Autowired
+    private Board board;
 
     @Test
     public void testTaskAdd() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
-        Board board = context.getBean(Board.class);
 
         board.getToDoList().addTask("drink water");
         board.getInProgressList().addTask("program the program");
         board.getDoneList().addTask("eat dinner");
-
 
         Assert.assertEquals("drink water", board.getToDoList().getTasks().get(0));
         Assert.assertEquals("program the program", board.getInProgressList().getTasks().get(0));
